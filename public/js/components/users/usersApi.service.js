4e5fb610efe43e33717134c0e80870f2046fd7bd
yazman12 <yazman12@gmail.com>
2015-10-11 21:26:48 -0400

    Added user service

---
+++ public/js/components/users/usersApi.service.js
@@ -0,0 +1,38 @@
+(function () {
+	angular
+		.module('users')
+		.factory(usersApi);
+
+	usersApi.$inject = ['$http'];
+
+	function usersApi ($http) {
+		var factory = {
+			getUser: getUser,
+			postUser: postUser,
+			putUser: putUser:
+			deleteUser: deleteUser
+		};
+
+		var usersApiPath = '/api/users/';
+
+		function getUser (userId) {
+			if (userId) {
+				var fullApiPath = usersApiPath + userId;
+				return $http.get(fullApiPath);
+			}
+			else {
+				return $http.get(usersApiPath);
+			}
+		}
+		function postUser (userData) {
+			return $http.post(usersApiPath, userData);
+		}
+		function putUser (userData) {
+			return $http.put(usersApiPath, userData);
+		}
+		function deleteUser (userId) {
+			var fullApiPath = usersApiPath + userId;
+			return $http.delete(fullApiPath);
+		}
+	}
+}());
