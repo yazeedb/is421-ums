745c1aa3dbd00cb79ab6971a6d46d1a711f3ff34
Afroze Khan <auk2@njit.edu>
2015-10-13 17:30:13 -0400

    fixed forgot controller

--- public/js/components/forgot/forgot.controller.js
+++ public/js/components/forgot/forgot.controller.js
@@ -3,11 +3,21 @@
 		.module('forgot')
 		.controller('forgotCtrl', forgotCtrl);
 
-	forgotCtrl.$inject = ['$scope','$state'];
+	forgotCtrl.$inject = ['$scope','$state','$http'];
 
-	function forgotCtrl ($scope, $state) {
+	function forgotCtrl ($scope, $state,$http) {
 		//make sure its injecting	
 		console.log("forgot controller injected");
+
+
+
+
+		 $http.post('http://anaik.us:8500/rest/user/login',{username:'auk2',password:'huknjk'}).then(function(res){
+        console.log(res.data);
+      })
+
+
+
 
 		$scope.forgotUsername = function () {
 			if(!formData.email){
