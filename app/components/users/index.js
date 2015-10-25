b8a9790418d949755186d5a5f94e63905b4eefad
Afroze Khan <auk2@njit.edu>
2015-10-25 12:49:16 -0400

    added single user crud routes

--- app/components/users/index.js
+++ app/components/users/index.js
@@ -10,5 +10,26 @@ function users (app, express) {
 		});
 	});
 
+	//crud on single user
+	usersApi.route('/:uid')
+
+		// get the user with that id
+		.get(function (req, res) {
+			//sql query or service
+		})
+
+		// update the user with this id
+		.put(function (req, res) {
+
+		})
+
+		// delete the user with this id
+		.delete(function (req, res) {
+
+		});
+
+
+
+
 	return usersApi;
 }
