1be7fcb5ce872c0c01a244793a696f157c080937
Afroze Khan <auk2@njit.edu>
2015-10-27 01:42:52 -0400

    added delete user route and query, it works

--- app/components/users/index.js
+++ app/components/users/index.js
@@ -10,6 +10,7 @@ var connection = mysql.createConnection(config.dbInfo);
 function users (app, express) {
 	var usersApi = express.Router();
 
+	// get all users
 	usersApi.get('/', function (req, res) {
 		var getUsers = dbHelper('SELECT * FROM is421_users');
 
@@ -21,6 +22,29 @@ function users (app, express) {
 		};
 	});
 
+// add a user
+	usersApi.put('/new', function (req, res) {
+
+			//get body data
+			var form = {};
+			form.username = req.body.username;
+			form.updateField = req.body.updateField;
+			form.updateValue = req.body.updateValue;
+
+			//form the query
+			var query = 'UPDATE is421_users SET ' + form.updateField + '=' +  connection.escape(form.updateValue)  + ' WHERE username = ' + connection.escape(form.username);
+
+			//execute query
+			connection.query(query, function(err, rows, fields) {
+			    if (err)
+					throw err;
+				
+				res.json(rows);
+			});
+			
+	}); //end put addUser
+
+	// update a user
 	usersApi.put('/update', function (req, res) {
 
 			//get body data
@@ -40,7 +64,28 @@ function users (app, express) {
 				res.json(rows);
 			});
 			
-	}); //end put updateUser 
+	}); //end put updateUser
+
+
+	//delete a user
+	usersApi.delete('/delete', function (req, res) {
+
+			//get body data
+			var form = {};
+			form.username = req.body.username;
+
+			//form the query
+			var query = 'DELETE FROM is421_users WHERE username = ' + connection.escape(form.username);
+
+			//execute query
+			connection.query(query, function(err, rows, fields) {
+			    if (err)
+					throw err;
+				
+				res.json(rows);
+			});
+			
+	}); //end put deleteUser 
 
 
 	//crud on single user
